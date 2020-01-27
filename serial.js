e0d08dee28abbbf9a4e42f5e07b87d3ff3930c47
Yaakov <git@yaakov.online>
2020-01-27 13:52:33 +1100

    Initial commit

---
+++ serial.js
@@ -0,0 +1,72 @@
+// For details of Uniden's serial protocol, see:
+//
+// - http://sc230-oss.sourceforge.net/important/SC230_Protocol.pdf
+// - http://info.uniden.com/twiki/pub/UnidenMan4/BCD536HPFirmwareUpdate/BCDx36HP_RemoteCommand_Specification_V1_05.pdf
+// - http://info.uniden.com/twiki/pub/UnidenMan4/BCD536HPFirmwareUpdate/Remote_Command_Plan_v0.17.pdf
+// - http://www.dslreports.com/r0/download/2169531~ba6babb3230f5fc5d656257aaed43916/UnidenProtocol.pdf
+
+async function serial_writeCommand(writer, text) {
+    await writer.write(text)
+    await writer.write('\r')
+}
+
+async function serial_readResponse(reader) {
+    let response = '';
+    while (!response.endsWith('\r')) {
+        let data = await reader.read();
+        response += data.value;
+    }
+    
+    // Trim the trailing newline
+    response = response.slice(0, response.length - 1);
+
+    return response.split(',');
+}
+
+async function serial_readWrite(reader, writer, command) {
+    await serial_writeCommand(writer, command);
+    let response = await serial_readResponse(reader);
+
+    var baseCommand = command.split(',')[0];
+    if (response[0] != baseCommand) {
+        throw new Error(`Command response mismatch: Expected ${baseCommand} but was ${response[0]}.`);
+    }
+
+    return response.slice(1);
+}
+
+async function serial_close(port, reader, writer) {
+    await writer.close();
+    await reader.cancel();
+    await port.close();
+}
+
+async function serial_openPort() {
+    const port = await navigator.serial.requestPort();
+
+    const baudRate =  parseInt(document.querySelector('select[name=baud]').value, 10)
+    await port.open({ baudrate: baudRate });
+
+    const decoder = new TextDecoderStream();
+    const inputDone = port.readable.pipeTo(decoder.writable);
+    const inputStream = decoder.readable;
+
+    const reader = inputStream.getReader();
+
+    const encoder = new TextEncoderStream();
+    const outputDone = encoder.readable.pipeTo(port.writable);
+    const outputStream = encoder.writable;
+
+    const writer = outputStream.getWriter();
+
+    return {
+        port: port,
+        reader: reader,
+        writer: writer,
+
+        write: serial_writeCommand.bind(undefined, writer),
+        read: serial_readResponse.bind(undefined, reader),
+        send: serial_readWrite.bind(undefined, reader, writer),
+        close: serial_close.bind(undefined, port, reader, writer),
+    }
+}
---
+++ site.js
@@ -0,0 +1,88 @@
+function init() {
+    if (!('serial' in navigator)) {
+        const notSupported = document.querySelectorAll('.noserial');
+        for (const element of notSupported) {
+            element.classList.remove('hidden');
+        }
+        return;
+    }
+}
+
+async function getSystemDetails(port, index) {
+    let details = await port.send(`SIN,${index}`);
+    return {
+        type: details[0],
+        name: details[1],
+        quickKey: details[2],
+        holdTime: details[3],
+        lockout: details[4],
+        reserved: details[5],
+        delay: details[6],
+        skip: details[7],
+        emergencyAlert: details[8],
+        revIndex: details[9],
+        fwdIndex: details[10],
+        channelGroupHead: details[11],
+        channelGroupTail: details[12],
+        sequence: details[13],
+    }
+}
+
+
+async function connect() {
+    const logArea = document.querySelector('div.log textarea');
+    logArea.value = 'Connecting...\r\n';
+
+    const log = (text) => logArea.value += text + '\r\n';
+
+    const port = await serial_openPort();
+
+    let response = await port.send('MDL');
+    log(`Model: ${response[0]}`);
+    
+    response = await port.send('PRG');
+    if (response[0] != 'OK')
+    {
+        log(`Error: Failed to enter programming mode: ${response[0]}`);
+        return;
+    }
+    log('Entered programming mode.')
+    log('');
+
+    response = await port.send('MEM');
+    log(`Memory used:${response[0]}%`);
+
+    response = await port.send('SCT');
+    const numSystems = parseInt(response[0], 10);
+    log(`${numSystems} systems detected.`);
+
+    if (numSystems > 0)
+    {
+        const head = await port.send('SIH');
+        const tail = await port.send('SIT');
+
+        let currentSystemIndex = parseInt(head, 10);
+        while (currentSystemIndex > 0)
+        {
+            let systemDetails = await getSystemDetails(port, currentSystemIndex);
+            log(`Found system: ${systemDetails.name}`);
+
+            let nextIndex = await port.send(`FWD,${currentSystemIndex}`);
+            currentSystemIndex = parseInt(nextIndex[0], 10);
+        }
+    }
+
+    response = await port.send('EPG');
+    if (response[0] != 'OK')
+    {
+        log(`Error: Failed to exit programming mode: ${response[0]}`);
+        return;
+    }
+    log('Exited programming mode.');
+    await port.close();
+    log('Disconnected.');
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    init();
+});
